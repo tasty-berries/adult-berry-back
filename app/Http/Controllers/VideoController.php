8566b3d566542b3c027af938f97de2d635509694
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-09-07 07:18:06 +0300

    Paginator

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -5,16 +5,26 @@ namespace App\Http\Controllers;
 use App\Http\Resources\VideoPageResource;
 use App\Http\Resources\VideoResource;
 use App\Models\Video;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 
 class VideoController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $data = $request->validate([
+            'search' => 'nullable|string|max:255'
+        ]);
+
         return VideoResource::collection(
             Video::query()
+                 ->when($data['search'] ?? false, fn(Builder $when) => $when
+                     ->where('title', 'LIKE', '%' . $data['search'] . '%')
+                 )
                  ->whereNotNull('video_id')
                  ->orderByDesc('views')
-                 ->get()
+                 ->orderBy('id')
+                 ->paginate(perPage: 40)
         );
     }
 
